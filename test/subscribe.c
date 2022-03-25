6e67819206431bb30364314c05706848f7987a0a
Evusviv <evusviv@gmail.com>
2022-03-25 18:36:16 +0100

    Create subscribe.c

---
+++ test/subscribe.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{	
+	
+	int x;
+	x = 10;
+	int y = x / 2;
+	// what is the value of y?
+	printf("The magic number is: %d\nThe value of x is %d\n", y, x);
+	return 0;
+}
